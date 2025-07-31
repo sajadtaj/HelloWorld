4265e865055c0b11bd9a01bf20930513cf83a3e7
sajadtaj <sajadtajco@gmail.com>
2025-07-31 21:23:17 +0330

    fix: change print for setep 3

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 name = input(" whats your name?")
 print(f"Hello {name}، Wellcome to my world ...")
-print(f"IS time to be happy . . . . ")
-print(f"IS time to be happy . . . . ")
+print(f"IS time to be happy . . . . try 1")
+print(f"IS time to be happy . . . . try 2")
+print(f"IS time to be happy . . . . try 3")
