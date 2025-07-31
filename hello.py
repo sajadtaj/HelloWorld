cccdfa9d3ee8683a850fbdd34f59da7ddb8659b8
sajadtaj <sajadtajco@gmail.com>
2025-07-31 21:25:58 +0330

    feat: Set new Feat

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ print(f"Hello {name}، Wellcome to my world ...")
 print(f"IS time to be happy . . . . try 1")
 print(f"IS time to be happy . . . . try 2")
 print(f"IS time to be happy . . . . try 3")
+
+
+# New feature
+print(f"New feature")
